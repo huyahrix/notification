a0bdc9bb01b89b224eb3c69f9752eccf95897270
Huy Phạm <infjnite@gmail.com>
2020-08-27 16:36:58 +0700

    delete device

--- app.js
+++ app.js
@@ -6,22 +6,21 @@ const morgan = require('morgan');
 const initRoutes = require('./config/routes.js');
 const WebSocket = require('ws');
 const dotenv = require('dotenv');
+const request = require('request');
+const bodyParser = require('body-parser');
 
 dotenv.config();
 
 const app = express();
 
-const bodyParser = require('body-parser');
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
-
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 // eslint-disable-next-line no-unused-vars
 app.use(function (error, req, res, next) {
     console.error(error);
     return res.json({ code: 'error', message: error.message, data: null, originalError: error });
 });
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 initRoutes(app);
 
@@ -33,20 +32,37 @@ app.set('socket', socket);
 app.set('clietns', clietns);
 
 socket.on('connection', (ws, req) => {
-    console.log(`new connection is established ${req.connection.remoteAddress}, number of connected clients : ${socket.clients.size}`);
 
     ws.id = req.headers['sec-websocket-key'];
     clietns[ws.id] = ws;
+    console.log(`new connection is established ${req.connection.remoteAddress} | id: ${ws.id} | number of connected clients : ${socket.clients.size}`);
 
     ws.send(JSON.stringify({
         code: 200, message: '', data: { SocketID: ws.id, Message: `new connection is established ${req.connection.remoteAddress}` }
     }));
 
     ws.on('close', () => {
-        delete clietns[ws.id];
-        // call api delete device
-        console.log('clietns lenght: ', Object.keys(clietns).length);
-        console.log(`client disconnected socket ${req.connection.remoteAddress}, number of connected clients : ${socket.clients.size}`);
+        try {
+            delete clietns[ws.id];
+            const options = {
+                'method': 'DELETE',
+                'url': 'https://apricot.diginet.com.vn/lpt-api-dev/device/del-by-token',
+                'headers': {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ tokenDevice: ws.id })
+
+            };
+            request(options, function (error, response) {
+                if (error) {
+                    console.error(`device/del-by-token | id: ${ws.id}  -> error: `, error);
+                }
+                console.log(`device/del-by-token | id: ${ws.id} -> response: `, response.body);
+            });
+            console.log(`client disconnected socket ${req.connection.remoteAddress}, number of connected clients : ${socket.clients.size} | clietns lenght: ${Object.keys(clietns).length} `);
+        } catch (error) {
+            console.error(error);
+        }
     });
 });
 
